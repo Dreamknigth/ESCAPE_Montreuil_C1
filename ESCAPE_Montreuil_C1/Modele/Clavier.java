a35f11abb20fbc3f26f9a469a6e63de2e7bda29d
yzhou <ying22835511@gmail.com>
2019-05-15 16:54:15 +0200

    ad

--- ESCAPE_Montreuil_C1/Modele/Clavier.java
+++
@@ -1,6 +0,0 @@
-package ESCAPE_Montreuil_C1.Modele;
-
-public class Clavier {
-	
-	
-}
--- ESCAPE_Montreuil_C1/Modele/blocks/terre.java
+++
@@ -1,9 +0,0 @@
-package ESCAPE_Montreuil_C1.Modele.blocks;
-
-public class terre extends ressource{
-
-	public terre(int resistance, boolean estDestructible, boolean estTraversable) {
-		super(10, true, false);
-	}
-
-}
