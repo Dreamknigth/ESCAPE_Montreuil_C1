8c7353efe1de9964a44f5a08c88e581283f3b018
yzhou <ying22835511@gmail.com>
2019-05-15 16:39:38 +0200

    mvt1.1

---
+++ Modele/blocks/arbre.java
@@ -0,0 +1,9 @@
+package ESCAPE_Montreuil_C1.Modele.blocks;
+
+public class arbre extends ressource{
+	public arbre(int resistance,int ressourceApporte, boolean estDestructible, boolean estTraversable) {
+		super(resistance, estDestructible, estDestructible);
+		
+	}
+	
+}
